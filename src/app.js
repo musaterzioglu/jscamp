e786ac5cada4ddb117845be6da55ccba7e12b884
musaterzioglu <42242692+musaterzioglu@users.noreply.github.com>
2021-10-19 00:04:18 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,27 @@
+//JS type safe değildir.
+console.log("Merhaba Kodlama.io")
+
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+
+{
+    let dolarDun = 9.10
+}
+const euroDun = 11.2
+//euroDun = 11
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi","Emlak Kredisi","Kamu Konut Kredisi"]
+for (let i = 0; i<konutKredileri.length;i++){console.log("<li>"+konutKredileri[i]+"<li>")}
+
+    
+
+console.log("<ul>")
+
+
+console.log(konutKredileri)
